5c6183ba2bee7fc7c62ed3c7fdfe05ee98f95a1f
2481849298 <2437586216@qq.com>
2024-09-16 21:49:20 +0800

    data

--- device.mk
+++ device.mk
@@ -6,3 +6,20 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Extra required packages
+
+PRODUCT_PACKAGES += \
+    libion
+    
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    android.hardware.fastboot@1.0-impl-mtk \
+    fastbootd
+    
+BUILD_FINGERPRINT := oplus/ossi/ossi:12/SP1A.210812.016/1694426926964:user/release-keys
--- twrp_OP4ABB.mk
+++ twrp_OP4ABB.mk
@@ -15,17 +15,6 @@ PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := OPPO A92S 5G
 PRODUCT_MANUFACTURER := OPPO
 
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Extra required packages
-PRODUCT_PACKAGES += \
-    libion
-    
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mtk \
-    fastbootd
-    
-BUILD_FINGERPRINT := oplus/ossi/ossi:12/SP1A.210812.016/1694426926964:user/release-keys
+
+
