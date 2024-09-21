46477c88e8eef7bbe5b4d7615ba672a3592dfd53
2481849298 <2437586216@qq.com>
2024-09-21 22:29:41 +0800

    666

--- device.mk
+++ device.mk
@@ -1,5 +1,10 @@
 DEVICE_PATH := device/oplus/OP4ABB
 
+BOARD_SHIPPING_API_LEVEL := 31
+BOARD_API_LEVEL := 31
+SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_TARGET_VNDK_VERSION := 31
 
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -26,3 +31,9 @@ PRODUCT_PACKAGES += \
     fastbootd
     
 BUILD_FINGERPRINT := oplus/ossi/ossi:12/SP1A.210812.016/1694426926964:user/release-keys
+
+# f2fs utilities
+PRODUCT_PACKAGES += \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
