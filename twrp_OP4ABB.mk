b7e34d09550767fd1fd1c72011b2443cfdac7ff0
2481849298 <2437586216@qq.com>
2024-10-07 05:28:37 +0800

    idi

--- twrp_OP4ABB.mk
+++ twrp_OP4ABB.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
